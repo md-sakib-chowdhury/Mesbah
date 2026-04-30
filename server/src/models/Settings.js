d5e33343b29c831cca1c75d04bbb9975507c3f88
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-04-30 09:48:05 +0600

    push new

--- server/src/models/Settings.js
+++ server/src/models/Settings.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
-
 const settingsSchema = new mongoose.Schema({
-    siteName: { type: String, default: 'Mesbah' },
+    siteName: { type: String, default: 'মেসবাহ' },
+    tagline: { type: String, default: 'তোমার পছন্দের মেস খোঁজো' },
+    logo: { type: String, default: '' },
+    heroBanner: { type: String, default: '' },
+    primaryColor: { type: String, default: '#6C63FF' },
+    contactEmail: { type: String, default: '' },
+    contactPhone: { type: String, default: '' },
+    footerText: { type: String, default: '' },
     maintenanceMode: { type: Boolean, default: false },
-    allowRegistration: { type: Boolean, default: true }
+    allowRegistration: { type: Boolean, default: true },
 }, { timestamps: true });
-
 module.exports = mongoose.model('Settings', settingsSchema);
