e3187f16b8614937dc3feb1a15e72197a697ae76
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-04-30 16:48:00 +0600

    11

---
+++ server/src/models/Area.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const areaSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    nameBn: { type: String, required: true },
+    isActive: { type: Boolean, default: true },
+}, { timestamps: true });
+module.exports = mongoose.model('Area', areaSchema);
