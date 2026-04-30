c1ffff6e88715b5132c67b22dd8e754507fd07e7
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-04-30 17:44:03 +0600

    23

--- server/src/models/Message.js
+++ server/src/models/Message.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const messageSchema = new mongoose.Schema({
-    room: { type: String, required: true },
-    sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    from: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    to: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     text: { type: String, required: true },
+    read: { type: Boolean, default: false },
 }, { timestamps: true });
 module.exports = mongoose.model('Message', messageSchema);
