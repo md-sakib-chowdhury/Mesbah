c718eb2a969f188985f120ca8bf9de94dbcc157f
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-05-01 21:42:55 +0600

    fix: add /api prefix

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API = axios.create({
     // এখানে আপনার প্রজেক্টের পুরো নামসহ লিঙ্কটি দিতে হবে
-    baseURL: 'https://mesbah-efvg.onrender.com',
+    baseURL: 'https://mesbah-efvg.onrender.com/api',
 });
 
 // টোকেন অটোমেটিক পাঠানোর জন্য এই অংশটুকু
