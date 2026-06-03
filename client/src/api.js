485dcfd423eb797404d49f671b80d34339e24e5e
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-06-03 11:17:38 +0600

    error

--- client/src/api.js
+++ client/src/api.js
@@ -1,16 +1,29 @@
+// import axios from 'axios';
+
+// const API = axios.create({
+//     // এখানে আপনার প্রজেক্টের পুরো নামসহ লিঙ্কটি দিতে হবে
+//     baseURL: 'https://mesbah-efvg.onrender.com/api',
+// });
+
+// // টোকেন অটোমেটিক পাঠানোর জন্য এই অংশটুকু
+// API.interceptors.request.use((req) => {
+//     const token = localStorage.getItem('token');
+//     if (token) {
+//         req.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return req;
+// });
+
+// export default API;
 import axios from 'axios';
 
 const API = axios.create({
-    // এখানে আপনার প্রজেক্টের পুরো নামসহ লিঙ্কটি দিতে হবে
-    baseURL: 'https://mesbah-efvg.onrender.com/api',
+    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api'
 });
 
-// টোকেন অটোমেটিক পাঠানোর জন্য এই অংশটুকু
-API.interceptors.request.use((req) => {
+API.interceptors.request.use(req => {
     const token = localStorage.getItem('token');
-    if (token) {
-        req.headers.Authorization = `Bearer ${token}`;
-    }
+    if (token) req.headers.Authorization = `Bearer ${token}`;
     return req;
 });
 
